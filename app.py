834fc085feedcd39be80db9b752c521173c841db
Vindhya R <vindhyagowda.r@gmail.com>
2026-04-01 11:12:55 +0530

    Format app.py for better readability

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def get_user_albums(user_id):
     return jsonify(res.json())
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000,host='0.0.0.0')
+    app.run( debug=True, port=5000 ,host='0.0.0.0')
